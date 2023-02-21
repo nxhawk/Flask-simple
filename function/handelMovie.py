8429d881c7f124061b2f202ff838588447a93721
haonhat0703 <haonhat2729@gmail.com>
2023-02-21 21:04:27 +0700

    add new movie

--- function/handelMovie.py
+++ function/handelMovie.py
@@ -10,3 +10,12 @@ def _getAllMovie():
         data = json.load(f)
         data = data['movies']
         return data
+
+
+def _addMovie(data):
+    with open(file, 'r', encoding='utf-8') as f:
+        data_list = json.load(f)
+        data_list['movies'].insert(0, {"name": data['name'], "rate": data['rate'],
+                                       "year": data['year'], "userAdd": data['user']})
+        open(file, "w", encoding='utf-8').write(json.dumps(data_list,
+                                                           indent=4, ensure_ascii=False))
