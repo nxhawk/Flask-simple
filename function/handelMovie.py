aa23bf68335f8413a91bbf20669b914b5474b677
haonhat0703 <haonhat2729@gmail.com>
2023-02-21 23:20:23 +0700

    edit and delete movie func

--- function/handelMovie.py
+++ function/handelMovie.py
@@ -16,6 +16,24 @@ def _addMovie(data):
     with open(file, 'r', encoding='utf-8') as f:
         data_list = json.load(f)
         data_list['movies'].insert(0, {"name": data['name'], "rate": data['rate'],
-                                       "year": data['year'], "userAdd": data['user']})
+                                       "year": data['year'], "userAdd": data['user'], "editBy": data['user']})
+        open(file, "w", encoding='utf-8').write(json.dumps(data_list,
+                                                           indent=4, ensure_ascii=False))
+
+
+def _delMovie(data):
+    with open(file, 'r', encoding='utf-8') as f:
+        data_list = json.load(f)
+        data_list['movies'].pop(int(data['stt']))
+        open(file, "w", encoding='utf-8').write(json.dumps(data_list,
+                                                           indent=4, ensure_ascii=False))
+
+
+def _editMovie(data):
+    with open(file, 'r', encoding='utf-8') as f:
+        data_list = json.load(f)
+        user = data_list['movies'][int(data['stt'])]['userAdd']
+        data_list['movies'][int(data['stt'])] = {"name": data['name'], "rate": data['rate'],
+                                                 "year": data['year'], "userAdd": user, "editBy": data['user']}
         open(file, "w", encoding='utf-8').write(json.dumps(data_list,
                                                            indent=4, ensure_ascii=False))
